e2c0c2f0222397a911a17c9d673c734b60b19f5a
Rick Newby <richard.newby@bongolearn.com>
2022-06-23 09:22:31 -0600

    Add merge sort test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include<time.h>
 #include <LinkedList.h>
+
+constexpr int MAX_VALUE = 100;
 
 int main(int argc, char *argv[])
 {
+    // Seed the random number generator
+    std::srand(time(0));
 
+    // Create a new list
+    LinkedListNode* head = new LinkedListNode(std::rand() % MAX_VALUE);
+
+    // Add 3 random elements to the list
+    for (int i = 1; i < 4; i++) {
+        LinkedListNode* element = new LinkedListNode(std::rand() % MAX_VALUE);
+        LinkedList::PushBack(head, element);
+    }
+
+    // Perform a merge sort on the new linked list
+    LinkedListNode* merged = LinkedList::MergeSort(head);
+
+    // Release dynamically allocated memory
+    LinkedListNode* current = merged;
+    while (current != nullptr) {
+        LinkedListNode* next = current->next;
+        delete current;
+        current = next;
+    }
+
+    return 0;
 }
